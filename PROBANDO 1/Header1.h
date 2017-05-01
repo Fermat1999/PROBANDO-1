5d79b6d02e9c13392216c593d38a000e19d77925
FERMAT <FERMAT@DESKTOP-DG0S5TL>
2017-04-30 19:15:31 -0500

    Cmbio vector

--- PROBANDO 1/Header1.h
+++ PROBANDO 1/Header1.h
@@ -1,2 +1,44 @@
 #pragma once
+#include <vector>
+#include "Header.h"
 
+using namespace std;
+
+class CVecProducto
+{
+private:
+	vector<CProducto*>*arrProducto;
+
+public:
+	CVecProducto();
+	~CVecProducto();
+	vector<CProducto*>*Get_arrProducto();
+	void Registrar_Producto(char *Nombre, char * Marca, double Precio_Neto, double Precio_Bruto);
+	void Eliminar_Producto(int pos);
+	void Eliminar_todo_Productos();
+	vector<CProducto*>*Reporte1();
+	vector<CProducto*>*Reporte2();
+	vector<CProducto*>*Reporte3();
+
+};
+
+CVecProducto::CVecProducto()
+{
+}
+CVecProducto::CVecProducto()
+{
+	arrProducto = new vector<CProducto*>();
+}
+
+CVecProducto::~CVecProducto()
+{
+}
+
+vector<CProducto*>* CVecProducto::Get_arrProducto()
+{
+	return this->arrProducto;
+}
+void CVecProducto::Registrar_Producto(char *Nombre, char * Marca, double Precio_Neto, double Precio_Bruto)
+{
+
+}
