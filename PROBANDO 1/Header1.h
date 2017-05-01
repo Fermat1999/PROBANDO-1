bc1611dadd1f8583c509d562512bad088ab846d9
FERMAT <FERMAT@DESKTOP-DG0S5TL>
2017-04-30 19:40:11 -0500

    1sasf

--- PROBANDO 1/Header1.h
+++ PROBANDO 1/Header1.h
@@ -15,7 +15,7 @@ public:
 	vector<CProducto*>*Get_arrProducto();
 	void Registrar_Producto(char *Nombre, char * Marca, double Precio_Neto, double Precio_Bruto);
 	void Eliminar_Producto(int pos);
-	void Eliminar_todo_Productos();
+	void Eliminar_todo_Productos(int pos);
 	vector<CProducto*>*Reporte1();
 	vector<CProducto*>*Reporte2();
 	vector<CProducto*>*Reporte3();
@@ -40,5 +40,16 @@ vector<CProducto*>* CVecProducto::Get_arrProducto()
 }
 void CVecProducto::Registrar_Producto(char *Nombre, char * Marca, double Precio_Neto, double Precio_Bruto)
 {
-
+	CProducto*objProducto = new CProducto(Nombre, Marca, Precio_Neto, Precio_Bruto);
+}
+void CVecProducto::Eliminar_Producto(int pos)
+{
+	arrProducto->erase(arrProducto->begin() + pos);
+}
+void CVecProducto::Eliminar_todo_Productos(int pos)//esta cagado esto creo///
+{
+	for ( int i = 0;  i<= 0 ; i=pos )
+	{
+		arrProducto->erase(arrProducto->begin() + pos);
+	}
 }
